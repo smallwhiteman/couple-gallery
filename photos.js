1573866fda15ad7f47456999b663a3cb6f9b4c97
reed <reed@reeddeMacBook-Air.local>
2026-02-04 18:55:03 +0800

    Add scooter ride photo

--- photos.js
+++ photos.js
@@ -9,4 +9,9 @@ const PHOTOS = [
     date: "某次特别的晚餐",
     caption: "给小公主准备的晚餐和花，希望以后的每一次庆祝都有你。",
   },
+  {
+    file: "scooter-ride.jpg",
+    date: "某个骑车回家的路上",
+    caption: "风很大，但抱着你的那一刻，我只觉得很安心。",
+  },
 ];
